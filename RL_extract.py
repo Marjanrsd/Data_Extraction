1094b991d4d1fd1a3591c8da97bb97754d2b4577
Marjan Rashidi <92228190+Marjanrsd@users.noreply.github.com>
2024-04-05 14:19:57 -0700

    Add files via upload
    final version

--- RL_extract.py
+++ RL_extract.py
@@ -19,30 +19,6 @@ import csv
 import glob
 
 
-# Read multiple txt files from a directory- first method
-# path=r"D:\lizzzz\Route Learning"
-# os.chdir(path)
-# def read_text_file(file_path):
-#     with open (file_path, "r") as f:
-#         print(f.read())
-        
-# for file in os.listdir():
-#     if file.endswith(".txt"):
-#         file_path=f"{path}\{file}"
-#         read_text_file(file_path)
-
-# # Read files using pandas- second method
-# folder_path=r"D:\lizzzz\Route Learning"
-# file_list=glob.glob(folder_path+"/*.txt")
-# main_dataframe = pd.DataFrame(pd.read_csv(file_list[0]))
-# for i in range(1, len(file_list)):
-#     data=pd.read_csv(file_list[i])
-#     df=pd.DataFrame(data)
-#     main_dataframe = pd.concat([main_dataframe,df],axis=1) 
-# print(main_dataframe)
-    
-
-# Read files 3rd method
 result_dir=r"D:\lizzzz\Route Learning2"
 all_csvs= os.listdir(result_dir)
 id_score_list=[]
@@ -50,21 +26,17 @@ id_score_list=[]
 for csv_f in all_csvs:
     id_=csv_f[0:4]
     print(id_)
-    # for i in range(len(all_csvs)):
     data=pd.read_csv(os.path.join(result_dir, csv_f))
-    num_err=data.count()
-    print(num_err)
+    print(data)
+    num_err=data.shape[0]
     id_score_list.append((id_, num_err)) 
-    
-
-with open("./RL.csv", 'w') as csv_f: 
+        
+ 
+with open("./RL2.csv", 'w') as csv_f: 
     csv_w = csv.writer(csv_f, delimiter = ',')   
     csv_w.writerows(id_score_list)
 
-    
-# with open(os.path.join(result_dir, csv_f), "r") as f:
-# reader = csv.reader(f, delimiter=",")
-        
+ 
     
 
     
